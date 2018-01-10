71f928abc17eb21d0b8187c26278a82a0d4a4d87
Oded Lazar <odedlaz@gmail.com>
2018-01-11 00:05:19 +0200

    Replace ScopeGuard with gsl::finally

--- include/utils.h
+++ include/utils.h
@@ -16,31 +16,7 @@ const suex::permissions::Group &WheelGroup();
 
 #define CONCAT_(a, b) a##b
 #define CONCAT(a, b) CONCAT_(a, b)
-#define DEFER(fn)                          \
-  ScopeGuard CONCAT(__defer__, __LINE__) { \
-    [&]() { fn; }                          \
-  }
-
-class ScopeGuard {
- public:
-  template <class Callable>
-  explicit ScopeGuard(Callable &&fn) : fn_(std::forward<Callable>(fn)) {}
-  ScopeGuard(ScopeGuard &other) noexcept : fn_(std::move(other.fn_)) {
-    other.fn_ = nullptr;
-  }
-
-  ~ScopeGuard() {
-    // must not throw
-    if (fn_) fn_();
-  }
-
-  ScopeGuard(const ScopeGuard &) = delete;
-
-  void operator=(const ScopeGuard &) = delete;
-
- private:
-  std::function<void()> fn_;
-};
+#define DEFER(fn) auto CONCAT(__defer__, __LINE__) = gsl::finally([&] { fn; });
 
 namespace suex::utils {
 std::string CommandArgsText(const std::vector<char *> &cmdargv);
