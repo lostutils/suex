4b431f68ca2a4800233a3c0e70338086f197a427
Oded Lazar <odedlaz@gmail.com>
2018-01-11 10:03:12 +0200

    Simplify rx::NamedFullMatch function

--- src/rx.cpp
+++ src/rx.cpp
@@ -3,26 +3,25 @@
 bool ::suex::utils::rx::NamedFullMatch(const re2::RE2 &rx,
                                        const std::string &line,
                                        suex::utils::rx::Matches *matches) {
-  std::vector<RE2::Arg> args(
-      static_cast<uint64_t>(rx.NumberOfCapturingGroups()));
-  std::vector<RE2::Arg *> ptrs(
-      static_cast<uint64_t>(rx.NumberOfCapturingGroups()));
-  std::vector<std::string> results(
-      static_cast<uint64_t>(rx.NumberOfCapturingGroups()));
+  int ngroups{rx.NumberOfCapturingGroups()};
 
-  /// Capture pointers to stack objects and result object in vector..
-  for (int i = 0; i < rx.NumberOfCapturingGroups(); ++i) {
-    /// Bind argument to string from vector.
-    args[i] = &results[i];
-    /// Save pointer to argument.
-    ptrs[i] = &args[i];
+  RE2::Arg args[ngroups];
+  const RE2::Arg *args_p[ngroups];
+  std::string groups[ngroups];
+
+  for (int i = 0; i < ngroups; ++i) {
+    /// Bind argument to string
+    args[i] = &groups[i];
+    /// bind argument pointer to arg address
+    args_p[i] = &args[i];
   }
 
-  bool matched =
-      re2::RE2::FullMatchN(line, rx, ptrs.data(), rx.NumberOfCapturingGroups());
-
-  for (auto &&kv : rx.NamedCapturingGroups()) {
-    matches->emplace(kv.first, results[kv.second - 1]);
+  bool matched = re2::RE2::FullMatchN(line, rx, &args_p[0], ngroups);
+  if (matched) {
+    for (auto &&kv : rx.NamedCapturingGroups()) {
+      matches->emplace(kv.first, groups[kv.second - 1]);
+    }
   }
+
   return matched;
 }
